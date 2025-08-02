3124e68411d17d8648d91e424865fa5212ebd2db
WhizJeff <jeff.amadorb@gmail.com>
2025-08-01 19:06:28 -0600

    Clean_Data

--- SistemaClientes/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaClientes.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-} 
